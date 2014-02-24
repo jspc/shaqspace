c2d247609c139d814a3a1c630429736219ec6e5c
jspc <james@zero-internet.org.uk>
2014-02-24 23:11:49 +0000

    Delete hosts too

--- lib/create.rb
+++ lib/create.rb
@@ -4,7 +4,13 @@ require 'fuggery/rackspace/spinup'
 class Create
   @queue = :shaq
   def self.perform u, k, fqdn, domain
+    if domain == 'fcuat.co.uk'
+      subdomains = %w(web bilcas db redis alpaca codas www api)
+    else
+      subdomains = %w(www)
+    end
+
     spinup = Fuggery::Rackspace::Spinup.new u, k
-    spinup.create fqdn, domain
+    spinup.create fqdn, domain, subdomains
   end
 end
--- routes/server_routes.rb
+++ routes/server_routes.rb
@@ -12,7 +12,30 @@ get '/server/:name' do
     @server      = session[:servers].server params[:name]
     erb :view
   rescue Exception=>e
-    @err = "Server #{params[:name]} doesn't seem to exist"
+    @err = "Server #{params[:name]} doesn't seem to exist or isn't a FQDN (You heathen!)"
     erb :err
+  end
+end
+
+get '/server/:name/delete' do
+  redirect '/login' unless session[:logged]
+  @name = params[:name]
+  erb :confirm_deletion
+end
+
+
+post '/server/:name/delete' do
+  redirect '/login' unless session[:logged]
+  if params[:confirmed] == 'true'
+    begin
+      @name = params[:name]
+      @host, @zone = Helpers::tld @name, session[:dns]
+      session[:dns].remove_all @name, @zone
+      session[:servers].remove @name
+      redirect '/'
+    rescue Exception=>e
+      @err = e
+      erb :err
+    end
   end
 end
